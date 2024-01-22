f46a2cd459fb0dcf208e528f8bd6ccc99776f843
Abraão Borges <abraao10clear@gmail.com>
2024-01-22 20:20:42 -0300

    Last update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,30 @@
-import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Abraão Borges"
-        content="Olá! Sou um entusiasta da tecnologia em transição de carreira, apaixonado por programação e pela arte de transformar problemas complexos em soluções criativas. Cada linha de código que escrevo é um passo em direção a um futuro onde a inovação e a resolução de desafios andam de mãos dadas. Estou ansioso para aplicar meu conhecimento e criar um impacto positivo por meio da tecnologia.👨‍💻"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Abraão Borges"
+            content="Olá! Sou um entusiasta da tecnologia em transição de carreira, apaixonado por programação e pela arte de transformar problemas complexos em soluções criativas. Cada linha de código que escrevo é um passo em direção a um futuro onde a inovação e a resolução de desafios andam de mãos dadas. Estou ansioso para aplicar meu conhecimento e criar um impacto positivo por meio da tecnologia.👨‍💻"
+          />
 
-      <Post
-        author="Ana Borges"
-        content="Code como se o mundo estivesse observando, mas crie como se ninguém estivesse julgando."
-      />
-
-      <Post
-        author="Abraão Borges"
-        content="Olá! Sou um entusiasta da tecnologia em transição de carreira, apaixonado por programação e pela arte de transformar problemas complexos em soluções criativas. Cada linha de código que escrevo é um passo em direção a um futuro onde a inovação e a resolução de desafios andam de mãos dadas. Estou ansioso para aplicar meu conhecimento e criar um impacto positivo por meio da tecnologia.👨‍💻"
-      />
+          <Post
+            author="Ana Borges"
+            content="Code como se o mundo estivesse observando, mas crie como se ninguém estivesse julgando."
+          />
+        </main>
+      </div>
     </div>
   );
 }
