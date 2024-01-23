38f3ff99361ffc00362aed154911497b31fc0cc2
Abraão Borges <abraao10clear@gmail.com>
2024-01-22 21:23:49 -0300

    Last update

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,7 +1,20 @@
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
-    <aside className={styles.sidebar}></aside>
-  )
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1621839673705-6617adf9e890?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <strong>Abraão Borges</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">Editar perfil</a>
+      </footer>
+    </aside>
+  );
 }
