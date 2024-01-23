52b4eb4d69dc746d572f2a69b02d1637b09b38ea
Abraão Borges <abraao10clear@gmail.com>
2024-01-22 23:38:04 -0300

    Last update

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { GithubLogo } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -6,14 +8,21 @@ export function Sidebar() {
       <img
         className={styles.cover}
         src="https://images.unsplash.com/photo-1621839673705-6617adf9e890?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/BorgesAbraao10.png"
+        />
+
         <strong>Abraão Borges</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar perfil</a>
+        <a href="https://github.com/BorgesAbraao10" target="blank">
+          <GithubLogo />
+          GitHub
+        </a>
       </footer>
     </aside>
   );
