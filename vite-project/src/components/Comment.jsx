1d861bb07e0e5cff258d4f34cdf5f609eb9e2010
Abraão Borges <abraao10clear@gmail.com>
2024-01-26 15:29:04 -0300

    Last update

--- vite-project/src/components/Comment.jsx
+++ vite-project/src/components/Comment.jsx
@@ -1,29 +1,35 @@
-import styles from './Comment.module.css'
+import styles from "./Comment.module.css";
+
+import { ThumbsUp, Trash } from "phosphor-react";
 
 export function Comment() {
   return (
-    <div className={styles.Comment}>
+    <div className={styles.comment}>
       <img src="https://github.com/BorgesAbraao10.png" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Diego Fernandes</strong>
-              <time title='25 de Janeiro às 12:03' dateTime= '2024-01-25 12:03'></time>
+              <time title="25 de Janeiro às 12:03" dateTime="2024-01-25 12:03">
+                Cerca de 1h atrás
+              </time>
             </div>
-            <button title='Deletar comentário'>
-              icone lixeira
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
             </button>
           </header>
           <p>Muito bom Devon 💻, parabens!! 👏👏👏👏</p>
         </div>
         <footer>
           <button>
-            icone aplaudir
-            Aplaudir <span>20</span>
+            Aplaudir
+            <ThumbsUp />
+            <span>20</span>
           </button>
         </footer>
       </div>
     </div>
-  )
+  );
 }
